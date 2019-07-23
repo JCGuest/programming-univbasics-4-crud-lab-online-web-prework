a06d2223414421fca0795c5c7496f433880cccc7
John Guest <jcguest90@gmail.com>
2019-07-23 04:42:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,11 +13,11 @@ array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  array
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
